aa66ef971adce96d5d9e494969e294acdc178804
SeunAkinbo <36178371+SeunAkinbo@users.noreply.github.com>
2023-10-18 04:32:57 +0100

    Update print_HEX.c

--- print_HEX.c
+++ print_HEX.c
@@ -22,8 +22,7 @@ int print_Hex(va_list val)
 	}
 	counter++;
 	array = malloc(sizeof(int) * counter);
-	if (array == NULL)
-		return (0);
+
 	for (i = 0; i < counter; i++)
 	{
 		array[i] = temp % 16;
